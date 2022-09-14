39a9331febd7eeed8d7a398b34f7561a0734bcf7
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-14 12:10:23 -0700

    Finish card componenet

--- src/Componenets/card.js
+++ src/Componenets/card.js
@@ -12,6 +12,20 @@ const myCard = () => {
             <a className='btn email'  href="mailto:shujashah786@gmail.com" target="_blank" rel="noopener noreferrer">Email</a>
             <a className= 'btn email' href= "https://linkedin.com/syedshujahussa" target="_blank" rel="noopener noreferrer"> LinkedIn</a>
         </div>
-        </>
+        <article className="about">
+            <h3 className="about-title">About</h3>
+            <p>Hi, I'm Syed Shuja Hussain. I'm a Full Stack Developer with a background in Computer Science. I'm passionate about building web applications and learning new technologies. I'm currently looking for a full time position as a Software Engineer.</p>
+        </article>
+
+        <article className= "Interest about">
+            <h3 className="about-title">Interests</h3>
+            <p>My interests include reading, writing, and learning new technologies. I'm also a big fan of the outdoors and enjoy hiking, camping, and rock climbing.</p>
+        </article>
+
+        <ul className='SocialMedia'>
+            <li><a href='http://twitter.com/SyedShujaHussa3'><img src={require('../Assets/Twitter.png')} alt='Twitter' /></a></li>
+            <li><a href='http://github.com/shuja-shah'><img src={require('../Assets/GitHub.png')} alt='GitHub' /></a></li>
+        </ul>
+        </> 
     )
 }
